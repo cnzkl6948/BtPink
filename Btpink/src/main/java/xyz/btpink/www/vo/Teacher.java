e3412eed056ecede973bcf2d4654fae52a2ba9bb
cnzkl6948 <cnzkl6948@naver.com>
2017-09-08 13:17:43 +0900

    no message

---
+++ Btpink/src/main/java/xyz/btpink/www/vo/Teacher.java
@@ -0,0 +1,34 @@
+package xyz.btpink.www.vo;
+
+public class Teacher extends Account{
+	private String rank;
+	
+	public Teacher(String memNo, String name, String type, String id, String pw, String email, String phone,
+			String status) {
+		super(memNo, name, type, id, pw, email, phone, status);
+	}
+
+	public Teacher(String memNo, String name, String type, String id, String pw, String email, String phone,
+			String status, String rank) {
+		super(memNo, name, type, id, pw, email, phone, status);
+		this.rank = rank;
+	}
+
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	@Override
+	public String toString() {
+		return "Teacher [rank=" + rank + "]";
+	}
+
+	
+	
+	
+
+}
