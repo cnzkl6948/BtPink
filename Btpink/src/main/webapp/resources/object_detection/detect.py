55ec5cbb6a30700bce73382d25ad86630fc03b92
btpink <karcmatica@gmail.com>
2017-09-18 13:23:30 +0900

    텐서플로 조정

--- Btpink/src/main/webapp/resources/object_detection/detect.py
+++ Btpink/src/main/webapp/resources/object_detection/detect.py
@@ -23,8 +23,8 @@ def load_image_into_numpy_array(image):
 #MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
 #MODEL_NAME = 'ssd_inception_v2_coco_11_06_2017'
 #MODEL_NAME = 'rfcn_resnet101_coco_11_06_2017'
-MODEL_NAME = 'faster_rcnn_resnet101_coco_11_06_2017'
-#MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017'
+#MODEL_NAME = 'faster_rcnn_resnet101_coco_11_06_2017'
+MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017'
 
 # frozen detection graph. 경로 설정
 PATH_TO_CKPT = 'models/' + MODEL_NAME + '/frozen_inference_graph.pb'
@@ -32,7 +32,7 @@ PATH_TO_CKPT = 'models/' + MODEL_NAME + '/frozen_inference_graph.pb'
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
-NUM_CLASSES = 90
+NUM_CLASSES = 200
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -96,7 +96,7 @@ while(True):
               
           num = len(np.squeeze(scores))
           for i in range(num):
-              if (np.squeeze(scores)[i] >= 0.75 and np.squeeze(classes).astype(np.int32)[i] == 1):
+              if (np.squeeze(scores)[i] >= 0.50 and np.squeeze(classes).astype(np.int32)[i] == 1):
                   cnt = cnt + 1
           # 검색결과를 사진에 표시.
           vis_util.visualize_boxes_and_labels_on_image_array(image_np,np.squeeze(boxes), np.squeeze(classes).astype(np.int32),np.squeeze(scores),category_index,use_normalized_coordinates=True,line_thickness=3)
